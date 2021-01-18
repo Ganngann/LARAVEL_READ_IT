3384f02f12aea5afe999fec71a7e57fee3ed0131
Pascal Lacroix <webiepscf@gmail.com>
2021-01-18 09:56:05 +0100

    Route par défaut (blog et posts.index)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Posts;
 
-Route::get('/', function() {
-  return view('templates.index');
-});
+// ROUTE PAR DEFAUT
+// PATTERN: /
+// CTRL: Posts
+// ACTION: index
+  Route::get('/', [Posts::class, 'index'])->name('blog');
+
+// LISTE DES POSTS
+// PATTERN: /posts
+// CTRL: Posts
+// ACTION: index
+  Route::get('/posts', [Posts::class, 'index'])->name('posts.index');
